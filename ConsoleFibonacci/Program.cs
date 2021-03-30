36774930987f486d94c2f253571f106514e98b6f
Jose-Daniel-Mar <daniel.mar@iest.edu.mx>
2021-03-29 23:53:13 -0600

    Primer commit

---
+++ ConsoleFibonacci/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese posicion inicial");
+            int posicion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese posiciones a mostrar");
+            int posiciones = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            int[] fi = new int[posicion + posiciones];
+            fi[0] = 0;
+            fi[1] = 1;
+            for (int i = 2; i < posicion + posiciones; i++)
+            {
+                fi[i] = fi[i - 2] + fi[i - 1];
+                
+            }
+            Console.Write("Sucesión de Fibonacci posicion inicial: " + posicion + ", posicion final: " + (posicion + posiciones) + "\n");
+            for (int i = posicion - 1; i < posicion + posiciones; i++)
+            {
+                Console.Write(fi[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleFibonacciReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleFibonacciReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ingrese posicion inicial");
+            int posicion = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("ingrese posiciones a mostrar hacia atras");
+            int posiciones = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            int[] fi = new int[posicion];
+            fi[0] = 0;
+            fi[1] = 1;
+            for (int i = 2; i < posicion; i++)
+            {
+                fi[i] = fi[i - 2] + fi[i - 1];
+
+            }
+            Console.Write("Sucesión de Fibonacci de reversa posicion inicial: " + posicion + ", posicion final: " + (posicion - posiciones) + "\n");
+            for (int i = posicion - 1; i > posiciones; i--)
+            {
+                Console.Write(fi[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
